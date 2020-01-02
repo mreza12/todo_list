dabeb8c1444b35aa78f1ba3db6c39fcb46370cc0
Reza Azimi <reza.azimi@prontocloud.com.au>
2020-01-03 00:08:54 +1100

    git action fix

--- app/todo_manager/urls.py
+++ app/todo_manager/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('task/', include('task.urls')),
     path('admin/', admin.site.urls),
     # path('accounts/', include('django.contrib.auth.urls'))
 ]
