08ef7e96489cbbe9b4d9b9f2169ff08ca78dac25
root <reza.trmm@gmail.com>
2020-01-04 15:44:33 +1100

    add unit tests to the task app

--- app/task/forms.py
+++ app/task/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from . import models
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+
 
 
 class categoryForm(forms.ModelForm):
@@ -20,4 +23,7 @@ class taskForm(forms.ModelForm):
             "note",
             "done",
         ]
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
+    helper.form_method = 'POST'
 
---
+++ app/test_settings.py
@@ -0,0 +1,4 @@
+
+from todo_manager.settings import *  # noqa
+
+# Override any settings required for tests here
